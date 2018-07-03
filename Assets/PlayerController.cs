1e316779554acca721e53e6a470a54c6b12f5667
Zack <zspencer@thatus.com>
2018-07-02 23:08:56 -0400

    Initial Commit of Files
    Basic files for the Audio Proof of Concept

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float mouseSensitivityHorizontal;
+    public float mouseSensitivityVertical;
+
+    public float moveSpeed;
+    public float strafeSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * strafeSpeed, 0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+        transform.Rotate(-Input.GetAxis("Mouse Y") * mouseSensitivityVertical, Input.GetAxis("Mouse X") * mouseSensitivityHorizontal, 0);
+ 	}
+}
